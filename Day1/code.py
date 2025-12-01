6d14ba9581b92d1962cad8cb5bb2b60ea786de41
AbdulRehman413 <abdul.rehman51048@gmail.com>
2025-12-01 16:14:58 +0500

    Day 1

--- Day1/code.py
+++ Day1/code.py
@@ -1,12 +1,32 @@
+
 def main():
-    with open("input.txt", "r") as f:
-        lines = f.readlines()
-    numbers = [int(line.strip()) for line in lines if line.strip()]
+    with open ("input.txt", "r") as f:
+        data = f.readlines()
+        
 
-    total = sum(numbers)
 
-    print("Total:", total)
+
+    dial = 50
+    counter = 0
+    for i in data:
+        i = i.strip()
+        number = int(i[1:])
+        if i.startswith("L"):
+            dial = (dial - number) % 100
+
+            
+        elif i.startswith("R"):
+            dial = (dial + number) % 100
+
+
+        if dial == 0:
+            counter += 1
+            
+
+    print(counter)
+
 
 
 if __name__ == "__main__":
     main()
+
